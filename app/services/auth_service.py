3f3c7e8de313d9b2f24c9a2e55a0ab81e102041b
Admincyanopy <info@cyanopy.com>
2026-03-16 11:08:24 +0530

    secure password hashing with bcrypt

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from app.models.user_model import User
 from app.schemas.user_schema import RegisterUser
 from fastapi import HTTPException
+from app.utils.security import hash_password, verify_password
 
 
 def register_user(db: Session, user: RegisterUser):
